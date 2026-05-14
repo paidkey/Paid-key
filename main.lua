080be505093b36dbee1f0eb72798611d38d050d3
paidkey <jayeshnaman123@gmail.com>
2026-05-14 21:55:03 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
-print("JN HH Gaming Loaded!")
+local ValidKeys = {
+    ["JN-Key-30days"] = true,
+    ["vip-key"] = true,
+    ["paid-user"] = true
+}
 
-loadstring(game:HttpGet("https://pastebin.com/raw/stzDSY53"))()
+local key = "JN-Key-30days" -- change for testing
+
+if ValidKeys[key] then
+    print("Key Valid!")
+
+    loadstring(game:HttpGet(
+        "https://pastebin.com/raw/stzDSY53"
+    ))()
+else
+    warn("Invalid Key")
+end
