f5e2ee42bc126d482b6fe019fe81ff77c3786077
paidkey <jayeshnaman123@gmail.com>
2026-05-14 21:56:41 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,14 +4,55 @@ local ValidKeys = {
     ["paid-user"] = true
 }
 
-local key = "JN-Key-30days" -- change for testing
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = game.CoreGui
 
-if ValidKeys[key] then
-    print("Key Valid!")
+local Frame = Instance.new("Frame")
+Frame.Size = UDim2.new(0,300,0,150)
+Frame.Position = UDim2.new(0.5,-150,0.5,-75)
+Frame.BackgroundColor3 = Color3.fromRGB(35,35,35)
+Frame.Parent = ScreenGui
 
-    loadstring(game:HttpGet(
-        "https://pastebin.com/raw/stzDSY53"
-    ))()
-else
-    warn("Invalid Key")
-end
+local Box = Instance.new("TextBox")
+Box.Size = UDim2.new(0.8,0,0,40)
+Box.Position = UDim2.new(0.1,0,0.25,0)
+Box.PlaceholderText = "Enter Key"
+Box.Text = ""
+Box.TextScaled = true
+Box.Parent = Frame
+
+local Button = Instance.new("TextButton")
+Button.Size = UDim2.new(0.8,0,0,40)
+Button.Position = UDim2.new(0.1,0,0.6,0)
+Button.Text = "Verify Key"
+Button.TextScaled = true
+Button.Parent = Frame
+
+local Status = Instance.new("TextLabel")
+Status.Size = UDim2.new(1,0,0,20)
+Status.Position = UDim2.new(0,0,0.88,0)
+Status.BackgroundTransparency = 1
+Status.TextColor3 = Color3.new(1,1,1)
+Status.TextScaled = true
+Status.Parent = Frame
+
+Button.MouseButton1Click:Connect(function()
+
+    local key = Box.Text
+
+    if ValidKeys[key] then
+
+        Status.Text = "Key Valid!"
+
+        task.wait(1)
+
+        ScreenGui:Destroy()
+
+        loadstring(game:HttpGet(
+            "https://pastebin.com/raw/stzDSY53"
+        ))()
+
+    else
+        Status.Text = "Invalid Key"
+    end
+end)
